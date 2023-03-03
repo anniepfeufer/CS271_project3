6e61d0069da54b743fa9bdadf672eb2e082d26f5
anniepfeufer <pfeufe_a1@denison.edu>
2023-03-02 21:39:39 -0500

    make hashtable

--- test_hash_table.cpp
+++ test_hash_table.cpp
@@ -1,1 +1,37 @@
+using namespace std;
+#include <iostream>
+#include "usecase.cpp"
 
+
+void test_get_key(){
+
+}
+
+void test_get_data(){
+
+}
+
+void test_insert(){
+
+}
+
+void test_remove(){
+
+}
+
+void test_member(){
+
+}
+
+
+
+int main(){
+    test_get_key();
+    test_get_data();
+    test_insert(); 
+    test_remove(); 
+    test_member();
+
+    cout<<"completed testing"<<endl;
+    return 0;
+}
