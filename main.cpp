74de933dfb9e1bc20a6b7adf09577f0caab2d557
anniepfeufer <pfeufe_a1@denison.edu>
2023-03-03 08:54:56 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,5 +4,26 @@
 using namespace std;
 
 int main(){
+    std::string file="logins.csv";
+    HashTable<string> *HT=create_table(file, 5);
+
+    
+    cout<<"Enter your username: ";
+    string usr;
+    cin>>usr;
+    cout<<endl<<"Enter your password: ";
+    string pwd;
+    cin>>pwd;
+    cout<<endl;
+    bool granted= login(HT, usr, pwd);
+    if (granted){
+        cout<<"Access Granted"<<endl;
+    }
+    else{
+        cout<<"Access Denied"<<endl;
+    }
+    
+
+
     return 0;
 }
